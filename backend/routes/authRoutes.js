f880eb3a9e733f863b7c9a88694e406574877d56
ATIK <atikul2585@gmail.com>
2025-03-05 16:14:02 +0600

    chore: update user authentication for login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -18,3 +18,21 @@ router.post("/signup", async(req, res) => {
         res.status(500).json({error: 'Registration failed'})
     }
 });
+
+//Login
+router.post("/login", async (req, res) => {
+    const {email, password } = req.body;
+    try{
+        const user = await User.findOne({where: {email}});
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            return res.status(401).json({error: "Invaild credintials"});
+        }
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expressIn: "1d"});
+        res.json({message: "Login successful", token});
+    }
+    catch (error){
+        res.status(500).json({error:"Login failed"});
+    }
+});
+
+module.export = router
